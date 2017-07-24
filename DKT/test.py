9efc4ca6ee91a5d315aece911175a2a1df269507
KevinHuuu <hcr14@mails.tsinghua.edu.cn>
2017-07-24 13:49:51 -0700

    testing option parser

--- DKT/test.py
+++ DKT/test.py
@@ -1,2 +1,22 @@
-a = 1
-b = 2
+from optparse import OptionParser 
+parser = OptionParser() 
+parser.add_option("--pdcl",'--asd','--qwe','--wer', action="store_true", 
+                  dest="pdcl", 
+                  default=False, 
+                  help="write pdbk data to oracle db") 
+parser.add_option("-z", "--zdbk", action="store_true", 
+                  dest="zdcl", 
+                  default=False, 
+                  help="write zdbk data to oracle db") 
+parser.add_option("--simple_index",action="store_true", dest="simple_index",\
+                 default=True,help="using simple index for training, no onehot,and difference between pages")
+
+
+
+(options, args) = parser.parse_args() 
+print(args)
+print(options)
+if options.pdcl==True: 
+    print ('pdcl is true') 
+if options.zdcl==True: 
+    print ('zdcl is true')
