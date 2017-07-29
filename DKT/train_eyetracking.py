d1c1d7d9e5f255efa8a65bcc5f0a4b57b72d8c77
KevinHuuu <hcr14@mails.tsinghua.edu.cn>
2017-07-28 17:43:29 -0700

    Use argparser instead of optparser, switch to Session1

--- DKT/train_eyetracking.py
+++ DKT/train_eyetracking.py
@@ -21,7 +21,7 @@ parser = argparse.ArgumentParser()
 
 parser.add_argument('--y_order', type=bool, default=True)
 
-#parser.add_argument('--simple_index', type=bool, default= False)
+parser.add_argument('--simple_index', type=bool, default= False)
 parser.add_argument('--simple_onehot', type=bool, default= False)
 parser.add_argument('--simple_continuous', type=bool, default= False)
 parser.add_argument('--complex_onehot', type=bool, default= False)
@@ -35,12 +35,77 @@ parser.add_argument('--RNN_mode', type=str, default= 'SimpleRNN')
 
 args = parser.parse_args()
 
-batch_size = args.batch_size # Adjust this parameter
+batch_size = args.batch_size 
 hidden_layer_size = args.hidden_layer_size
 learning_rate = args.learning_rate
 optimizer_mode = args.optimizer_mode
 RNN_mode = args.RNN_mode
 
+
+
+
+# This list is for Session 1, 36+9 students in total
+cross_val_list = [
+    [[0, 1, 3, 4, 5, 7, 8, 9, 10, 11, 12, 15, 16, 17, 18, 20, 21, 22, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 40, 41, 42, 44], 
+     [2, 6, 13, 14, 19, 23, 38, 39, 43]], 
+    [[0, 1, 2, 3, 5, 6, 8, 10, 11, 12, 13, 14, 15, 17, 18, 19, 20, 21, 22, 23, 24, 25, 27, 28, 29, 30, 31, 32, 34, 35, 36, 37, 38, 39, 41, 43], 
+     [4, 7, 9, 16, 26, 33, 40, 42, 44]], 
+    [[0, 1, 2, 3, 4, 6, 7, 9, 12, 13, 14, 16, 17, 19, 21, 22, 23, 25, 26, 27, 28, 29, 30, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44], 
+     [5, 8, 10, 11, 15, 18, 20, 24, 31]], 
+    [[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 23, 24, 25, 26, 28, 31, 33, 34, 35, 38, 39, 40, 42, 43, 44], 
+     [0, 22, 27, 29, 30, 32, 36, 37, 41]], 
+    [[0, 2, 4, 5, 6, 7, 8, 9, 10, 11, 13, 14, 15, 16, 18, 19, 20, 22, 23, 24, 26, 27, 29, 30, 31, 32, 33, 36, 37, 38, 39, 40, 41, 42, 43, 44], 
+     [1, 3, 12, 17, 21, 25, 28, 34, 35]]]
+
+train_val_data = [
+ '/research/atoms/Session1/2014 F Session 1/atoms38_imputed.csv',
+ '/research/atoms/Session1/2014 F Session 1/atoms26_imputed.csv',
+ '/research/atoms/Session1/2014 F Session 1/atoms14_imputed.csv',
+ '/research/atoms/Session1/2014 F Session 1/atoms49_imputed.csv',
+ '/research/atoms/Session1/2014 F Session 1/atoms18_imputed.csv',
+ '/research/atoms/Session1/2014 F Session 1/atoms28_imputed.csv',
+ '/research/atoms/Session1/2014 F Session 1/atoms23_imputed.csv',
+ '/research/atoms/Session1/2014 F Session 1/atoms9_imputed.csv',
+ '/research/atoms/Session1/2014 F Session 1/atoms33_imputed.csv',
+ '/research/atoms/Session1/2014 F Session 1/atoms4_imputed.csv',
+ '/research/atoms/Session1/2014 F Session 1/atoms10_imputed.csv',
+ '/research/atoms/Session1/2014 I Session 1/atoms7_imputed.csv',
+ '/research/atoms/Session1/2014 I Session 1/atoms27_imputed.csv',
+ '/research/atoms/Session1/2014 I Session 1/atoms3_imputed.csv',
+ '/research/atoms/Session1/2014 I Session 1/atoms13_imputed.csv',
+ '/research/atoms/Session1/2014 I Session 1/atoms5_imputed.csv',
+ '/research/atoms/Session1/2014 I Session 1/atoms35_imputed.csv',
+ '/research/atoms/Session1/2014 I Session 1/atoms46_imputed.csv',
+ '/research/atoms/Session1/2014 I Session 1/atoms11_imputed.csv',
+ '/research/atoms/Session1/2014 I Session 1/atoms29_imputed.csv',
+ '/research/atoms/Session1/2014 I Session 1/atoms30_imputed.csv',
+ '/research/atoms/Session1/2014 I Session 1/atoms2_imputed.csv',
+ '/research/atoms/Session1/2014 S Session 1/atoms31_imputed.csv',
+ '/research/atoms/Session1/2014 S Session 1/atoms47_imputed.csv',
+ '/research/atoms/Session1/2014 S Session 1/atoms44_imputed.csv',
+ '/research/atoms/Session1/2014 S Session 1/atoms15_imputed.csv',
+ '/research/atoms/Session1/2014 S Session 1/atoms19_imputed.csv',
+ '/research/atoms/Session1/2014 S Session 1/atoms37_imputed.csv',
+ '/research/atoms/Session1/2014 S Session 1/atoms50_imputed.csv',
+ '/research/atoms/Session1/2014 S Session 1/atoms6_imputed.csv',
+ '/research/atoms/Session1/2014 S Session 1/atoms12_imputed.csv',
+ '/research/atoms/Session1/2014 S Session 1/atoms36_imputed.csv',
+ '/research/atoms/Session1/2014 S Session 1/atoms43_imputed.csv',
+ '/research/atoms/Session1/2014 S Session 1/atoms41_imputed.csv',
+ '/research/atoms/Session1/2014 SF Session 1/atoms22_imputed.csv',
+ '/research/atoms/Session1/2014 SF Session 1/atoms42_imputed.csv',
+ '/research/atoms/Session1/2014 SF Session 1/atoms17_imputed.csv',
+ '/research/atoms/Session1/2014 SF Session 1/atoms39_imputed.csv',
+ '/research/atoms/Session1/2014 SF Session 1/atoms8_imputed.csv',
+ '/research/atoms/Session1/2014 SF Session 1/atoms45_imputed.csv',
+ '/research/atoms/Session1/2014 SF Session 1/atoms21_imputed.csv',
+ '/research/atoms/Session1/2014 SF Session 1/atoms20_imputed.csv',
+ '/research/atoms/Session1/2014 SF Session 1/atoms1_imputed.csv',
+ '/research/atoms/Session1/2014 SF Session 1/atoms32_imputed.csv',
+ '/research/atoms/Session1/2014 SF Session 1/atoms34_imputed.csv']
+
+'''
+# This list is for Session 2, 20+5 students in total
 cross_val_list = [[[ 5,  6,  7,  8,  9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21,
        22, 23, 24],[0, 1, 2, 3, 4]],
 [[ 0,  1,  2,  3,  4, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21,
@@ -55,23 +120,13 @@ cross_val_list = [[[ 5,  6,  7,  8,  9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 
 train_val_data = ['atoms31_imputed.csv', 'atoms21_imputed.csv', 'atoms28_imputed.csv', 'atoms15_imputed.csv', 'atoms20_imputed.csv', 'atoms37_imputed.csv', 'atoms18_imputed.csv', 'atoms36_imputed.csv', 'atoms07_imputed.csv', 'atoms6_imputed.csv', 'atoms1_imputed.csv', 'atoms38_imputed.csv', 'atoms45_imputed.csv', 'atoms27_imputed.csv', 'atoms9_imputed.csv', 'atoms22_imputed.csv', 'atoms4_imputed.csv', 'atoms8_imputed.csv', 'atoms29_imputed.csv', 'atoms14_imputed.csv', 'atoms12_imputed.csv', 'atoms17_imputed.csv', 'atoms43_imputed.csv', 'atoms26_imputed.csv', 'atoms19_imputed.csv']
 
 test_data = ['atoms11_imputed.csv', 'atoms32_imputed.csv', 'atoms41_imputed.csv', 'atoms33_imputed.csv', 'atoms39_imputed.csv', 'atoms10_imputed.csv', 'atoms23_imputed.csv']
-
-
-
-
-root = '/research/atoms/Session2'
-imputed_list = []
-for dirpath,dirnames,filenames in os.walk(root):
-    for filename in filenames:
-        if filename.endswith('_imputed.csv'):
-            imputed_list.append(dirpath + '/' + filename)
+'''
 
 total_problems = []
 total_answers = []
 problem_ID = 0 # make each question to be an ID
 problem_set = {} # unique problem type
 answer_set = {'CORRECT':1, 'INCORRECT':0}
-print ('The num of students is ', len(imputed_list))
 
 total_position = []
 total_answers = []
@@ -79,14 +134,14 @@ total_name = []
 total_index = []
 total_time_stamp = []
 total_page_num = []
-total_SRquestion = []
+    total_SRquestion = []
 
 '''For y_order'''
 SRquestion_set = set()
 SRquestion_ID = 0
 SRquestion_dict = {}
 
-imputed_file = imputed_list[0]
+
 x_min = float("inf")
 y_min = float("inf")
 x_max = 0 # initialization
@@ -95,79 +150,82 @@ w_size = 5
 h_size = 5
 epoch = 50
 
-screen_num = 3
+standard = [] # standard is for choosing certain Screen or page. 
+
 ''' Starts processing data'''    
-for imputed_file in imputed_list:
+for imputed_file in train_val_data:
     if imputed_file[-19] == '/':
         student_name = imputed_file[-18:]
     else:
         student_name = imputed_file[-19:]
-    if student_name in train_val_data:
-        print(student_name)
-        student = pd.read_csv(imputed_file, low_memory=False)
-        total_name.append(student_name)
-        
-        total_page_num.append(list(student.Representation_Number_Overall[student.Screen_Number<=screen_num]))
-        
-        total_position.append([student.L_Raw_X[student.Screen_Number<=screen_num],\
-                               student.L_Raw_Y[student.Screen_Number<=screen_num]])
-        
-        '''Now ignore unimportant values in questions'''
-        student.loc[(student['Response']!='CORRECT') & (student['Response']!='INCORRECT'),'question'] = 'ignore'
-        student.loc[(student.question =='_root') | (student.question == 'done'),'question'] = 'ignore'
-        
-        '''Create SRquestion'''
-        
-        '''Change type of numbers for creating SRquestion'''
-        student.Screen_Number = student.Screen_Number.astype(str)
-        student.Representation_Number_Within_Screen = student.Representation_Number_Within_Screen.astype(str)
-        
-        '''Create column named SRquestion_name'''
-        student['SRquestion_name'] = student.Screen_Number + \
-                        student.Representation_Number_Within_Screen +\
-                        student.question
-                
-        '''Change type of numbers back'''
-        student.Screen_Number = student.Screen_Number.astype(float)
-        student.Representation_Number_Within_Screen = student.Representation_Number_Within_Screen.astype(float)     
-        
-        '''Update the SRquestion set and dict'''
-        # We only want SRquestion_set within certain Screen
-        SRquestion_set.update(student.loc[(student.question!= 'ignore') &\
-                                  (student.Screen_Number<=screen_num),'SRquestion_name']) 
-        
-        # SRquestion_ID starts from 1. SRquestion_ID == 0 means padding.
-        for SRquestion_name in SRquestion_set: 
-            if SRquestion_name not in SRquestion_dict:
-                SRquestion_ID += 1
-                SRquestion_dict.update({SRquestion_name: SRquestion_ID})
-        student['SRquestion_ID'] = 0 #initialize new column.
-        
-        tmp_condition = (student.question!='ignore')&(student.Screen_Number<=screen_num)
-        student.loc[tmp_condition,'SRquestion_ID'] = student.loc[tmp_condition,'SRquestion_name'].\
-                                                     apply(lambda x:SRquestion_dict[x])    
-        tmp_condition = []
-        
-        total_SRquestion.append(student['SRquestion_ID'][student.Screen_Number<= screen_num])
-        
-        student.Response = student.Response.replace(float('nan'),-1)
-        student.Response = student.Response.replace('HINT', -1)
-        student.Response = student.Response.replace('CORRECT',1)
-        student.Response = student.Response.replace('INCORRECT',0)
-        
-        total_answers.append(student.Response[student.Screen_Number<= screen_num])
-        total_time_stamp.append(student[student.Screen_Number<= screen_num].iloc[:,0])
-        
-        '''To avoid(0,0) calculated as the min value'''
-        student.L_Raw_X = student.L_Raw_X.replace(0.0,float('inf'))
-        student.L_Raw_Y = student.L_Raw_Y.replace(0.0,float('inf'))
-        x_min = min(x_min,min(student.L_Raw_X[student.Screen_Number<= screen_num]))
-        y_min = min(y_min,min(student.L_Raw_Y[student.Screen_Number<= screen_num]))
-        student.L_Raw_X = student.L_Raw_X.replace(float('inf'),0.0)
-        student.L_Raw_Y = student.L_Raw_Y.replace(float('inf'),0.0)
-        
-        x_max = max(x_max,max(student.L_Raw_X[student.Screen_Number<= screen_num]))
-        y_max = max(y_max,max(student.L_Raw_Y[student.Screen_Number<= screen_num]))
+    print(imputed_file)
+    
+    student = pd.read_csv(imputed_file, low_memory=False)
+    standard = (student.Stimulus == 'bl i.jpg')
+    total_name.append(student_name)
+
+    total_page_num.append(list(student.Representation_Number_Overall[standard]))
+
+    total_position.append([student.L_Raw_X[standard],\
+                           student.L_Raw_Y[standard]])
+
+    '''Now ignore unimportant values in questions'''
+    student.loc[(student['Response']!='CORRECT') & (student['Response']!='INCORRECT'),'question'] = 'ignore'
+    student.loc[(student.question =='_root') | (student.question == 'done'),'question'] = 'ignore'
+
+    '''Create SRquestion'''
+
+    '''Change type of numbers for creating SRquestion'''
+    #student.Screen_Number = student.Screen_Number.astype(str)
+    student.Representation_Number_Overall = student.Representation_Number_Overall.astype(str)
+
+    '''Create column named SRquestion_name'''
+    #student['SRquestion_name'] = student.Screen_Number + \
+                    #student.Representation_Number_Within_Screen +\
+                    #student.question
+    student['SRquestion_name'] = student.Representation_Number_Overall + student.question
+
+    '''Change type of numbers back'''
+    #student.Screen_Number = student.Screen_Number.astype(float)
+    student.Representation_Number_Overall = student.Representation_Number_Overall.astype(float)     
+
+    '''Update the SRquestion set and dict'''
+    # We only want SRquestion_set within certain Screen
+    SRquestion_set.update(student.loc[(student.question!= 'ignore') &\
+                              (standard),'SRquestion_name']) 
+
+    # SRquestion_ID starts from 1. SRquestion_ID == 0 means padding.
+    for SRquestion_name in SRquestion_set: 
+        if SRquestion_name not in SRquestion_dict:
+            SRquestion_ID += 1
+            SRquestion_dict.update({SRquestion_name: SRquestion_ID})
+    student['SRquestion_ID'] = 0 #initialize new column.
+
+    tmp_condition = (student.question!='ignore')&(standard)
+    student.loc[tmp_condition,'SRquestion_ID'] = student.loc[tmp_condition,'SRquestion_name'].\
+                                                 apply(lambda x:SRquestion_dict[x])    
+    tmp_condition = []
+
+    total_SRquestion.append(student['SRquestion_ID'][standard])
+
+    student.Response = student.Response.replace(float('nan'),-1)
+    student.Response = student.Response.replace('HINT', -1)
+    student.Response = student.Response.replace('CORRECT',1)
+    student.Response = student.Response.replace('INCORRECT',0)
+
+    total_answers.append(student.Response[standard])
+    total_time_stamp.append(student[standard].iloc[:,0])
+
+    '''To avoid(0,0) calculated as the min value'''
+    student.L_Raw_X = student.L_Raw_X.replace(0.0,float('inf'))
+    student.L_Raw_Y = student.L_Raw_Y.replace(0.0,float('inf'))
+    x_min = min(x_min,min(student.L_Raw_X[standard]))
+    y_min = min(y_min,min(student.L_Raw_Y[standard]))
+    student.L_Raw_X = student.L_Raw_X.replace(float('inf'),0.0)
+    student.L_Raw_Y = student.L_Raw_Y.replace(float('inf'),0.0)
+
+    x_max = max(x_max,max(student.L_Raw_X[standard]))
+    y_max = max(y_max,max(student.L_Raw_Y[standard]))
 print('x_max is ',x_max)
 print('y_max is ',y_max)
 assert len(total_position) == len(total_answers)
@@ -426,7 +484,7 @@ for indexes in cross_val_list:
         
         train_input = np.eye(int(1+max_page*(max_index+1)))[np.array(train_input,dtype = int)] # onehot
         train_truth = (np.array(train_truth))[:,:,np.newaxis]
-        val_input = np.eye(int(1+max_page(max_index+1)))[np.array(val_input,dtype = int)] #onehot
+        val_input = np.eye(int(1+max_page*(max_index+1)))[np.array(val_input,dtype = int)] #onehot
         val_truth = (np.array(val_truth))[:,:,np.newaxis]
         
         
@@ -446,4 +504,6 @@ for indexes in cross_val_list:
         print('Sth wrong about y_order when applying to model')
 
         
+
+
 
